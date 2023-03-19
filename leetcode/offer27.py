166330ea411a85fa10a9828f1bf178f80c1738f6
LAPTOP-1A1P0DLN\m0815 <m081550370@163.com>
2023-03-19 22:07:44 +0800

    new file:   leetcode/offer27.py

---
+++ leetcode/offer27.py
@@ -0,0 +1,7 @@
+class Solution:
+    def mirrorTree(self,root:'TreeNode')-> TreeNode:
+        if not root: return
+        tmp= root.left
+        root.left=self.mirrorTree(root.right)
+        root.right=self.mirrorTree(tmp)
+        return root
