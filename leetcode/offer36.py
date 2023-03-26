ae1c1bbb61b5aa4f2a2d76dd43f8d56c186cc83c
LAPTOP-1A1P0DLN\m0815 <m081550370@163.com>
2023-03-26 21:19:51 +0800

    new file:   leetcode/offer36.py

---
+++ leetcode/offer36.py
@@ -0,0 +1,19 @@
+class Solution:
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        def dfs(cur):
+            if not cur:
+                return
+            dfs(cur.left)
+            if self.pre:
+                self.pre.right, cur.left = cur, self.pre
+            else:
+                self.head = cur
+            self.pre = cur
+            dfs(cur.right)
+
+        if not root:
+            return
+        self.pre = None
+        dfs(root)
+        self.head.left, self.pre.right = self.pre, self.head
+        return self.head
