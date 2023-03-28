f7a6202c49fb2d1b92f5824254772d2b0ec45073
LAPTOP-1A1P0DLN\m0815 <m081550370@163.com>
2023-03-28 20:18:28 +0800

    大学习

--- da_xue_xi/test.py
+++ da_xue_xi/test.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
-# 创建一个 DataFrame
-data = {'name': ['Alice', 'Bob', 'Cathy', 'David', 'Alice'],
-        'age': [25, 26, 27, 28, 25],
-        'city': ['Beijing', 'Shanghai', 'Guangzhou', 'Shenzhen', 'Beijing']}
+# 生成示例数据
+data = {'col1': ['A', 'B', 'C', 'A', 'A', 'B', 'C', 'C', 'D'],
+        'col2': [1, 2, 3, 4, 5, 6, 7, 8, 9]}
 df = pd.DataFrame(data)
 
-# 使用 duplicated 方法检查 DataFrame 中的重复行
-duplicated = df.duplicated()
+# 使用 groupby() 方法计数，并将结果输出到新的一列
+df['count'] = df.groupby(['col1']).transform('count')
 
+# 保证第一列没有重复值
+df = df.drop_duplicates(['col1'])
+
+# 打印结果
 print(df)
