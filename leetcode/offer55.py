dc842a47f7a6d1bfe4d493c0d1d8dedd2a415014
manshushu <42796030+manshushu@users.noreply.github.com>
2023-04-01 11:12:24 +0800

    leetcode+former

---
+++ leetcode/offer55.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self,root:TreeNode)-> int:
+        if not root: return 0
+        queue,res=[root],0
+        while queue:
+            tmp=[]
+            for node in queue:
+                if node.left: tmp.append(node.left)
+                if node.right: tmp.append(node.right)
+            queue=tmp
+            res+=1
+        return res
