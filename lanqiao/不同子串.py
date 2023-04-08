7fcae092890e1212b586fba4aba49c4b04abe281
manshushu <42796030+manshushu@users.noreply.github.com>
2023-04-08 12:14:01 +0800

    整理

--- lanqiao/不同子串.py
+++
@@ -1,8 +0,0 @@
-input_str = input()
-num = len(input_str)
-searched = []
-for i in range(num):
-    for j in range(num):
-        if str(input_str[j:j+i:]) not in searched:
-            searched.append(str(input_str[j:j+i:]))
-print(len(searched))
